621dc4c3158c7f4c8845c08a08c4b8e0366ab00b
Henrique França <euhenriquefranca@gmail.com>
2021-07-04 16:23:43 -0300

    fit and styling component card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Button from "../Button";
+import More from "../More";
 
 import "./styles.css";
 
 export default function Card() {
   return (
     <div className="card-container">
-      <p className="summary">Seu resumo</p>
+      <div className="header">
+        <span className="summary">Seu resumo</span>
+        <div className="more-svg">
+          <More />
+        </div>
+      </div>
       <p className="invested">Valor investido</p>
       <p className="value">R$ 3.200.876,00</p>
-      <p className="profitability">Rentabilidade/mês</p>
-      <p className="cdi">CDI</p>
-      <p className="gain">Ganho/mês</p>
+      <div className="profitability-container">
+        <p className="profitability">Rentabilidade/mês</p>
+        <p className="profitability-value">2,767%</p>
+      </div>
+      <div className="cdi-container">
+        <p className="cdi">CDI</p>
+        <p className="cdi-value">3,45%</p>
+      </div>
+      <div className="gain-container">
+        <p className="gain">Ganho/mês</p>
+        <p className="gain-value">R$ 1833,23</p>
+      </div>
+      <div className="line"></div>
       <div className="btn">
         <Button title="VER MAIS" />
       </div>
---
+++ src/components/More/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MoreSvg from "../../assets/more.svg";
+
+export default function More() {
+  return <img alt="More" src={MoreSvg} />;
+}
