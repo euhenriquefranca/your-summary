fc099a2c4200a9c0150cd13ef303c0525d683316
Henrique França <euhenriquefranca@gmail.com>
2021-07-05 09:01:23 -0300

    ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import Card from "./components/Card";
 import { summary } from "./graphql/summary";
-import { gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    initial();
-  }, []);
-
-  function initial() {
-    summary
-      .query({
-        query: gql`
-          query MyQuery {
-            wealthSummary {
-              cdi
-              gain
-              hasHistory
-              id
-              profitability
-              total
-            }
-          }
-        `,
-      })
-      .then((res) => console.log(res));
-  }
-
   return (
-    <div className="container">
-      <Card />
-    </div>
+    <ApolloProvider client={summary}>
+      <div className="container">
+        <Card />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,36 +1,35 @@
 import React from "react";
-import Button from "../Button";
-import More from "../More";
+import { gql, useQuery } from "@apollo/client";
+import Item from "../Item";
 
 import "./styles.css";
 
+const GET_YOUR_SUMMARY = gql`
+  query MyQuery {
+    wealthSummary {
+      cdi
+      gain
+      hasHistory
+      id
+      profitability
+      total
+    }
+  }
+`;
+
+const ContainerSummary = ({ children }) => (
+  <div className="container">{children}</div>
+);
 export default function Card() {
+  const { loading, data } = useQuery(GET_YOUR_SUMMARY);
+
+  if (loading) return <ContainerSummary>Carregando...</ContainerSummary>;
+
   return (
-    <div className="card-container">
-      <div className="header">
-        <span className="summary">Seu resumo</span>
-        <div className="more-svg">
-          <More />
-        </div>
-      </div>
-      <p className="invested">Valor investido</p>
-      <p className="value">R$ 3.200.876,00</p>
-      <div className="profitability-container">
-        <p className="profitability">Rentabilidade/mês</p>
-        <p className="profitability-value">2,767%</p>
-      </div>
-      <div className="cdi-container">
-        <p className="cdi">CDI</p>
-        <p className="cdi-value">3,45%</p>
-      </div>
-      <div className="gain-container">
-        <p className="gain">Ganho/mês</p>
-        <p className="gain-value">R$ 1833,23</p>
-      </div>
-      <div className="line"></div>
-      <div className="btn">
-        <Button title="VER MAIS" />
-      </div>
-    </div>
+    <ContainerSummary>
+      {data.wealthSummary.map((item, index) => (
+        <Item key={index} item={item} />
+      ))}
+    </ContainerSummary>
   );
 }
